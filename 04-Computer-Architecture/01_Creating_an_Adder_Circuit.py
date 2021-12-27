cfb8e9d11a08e8792ecdd329bc88b187b3429f17
Mica <92948781+miccaelaa@users.noreply.github.com>
2021-12-27 18:44:47 -0300

    Update 01_Creating_an_Adder_Circuit.py

--- 04-Computer-Architecture/01_Creating_an_Adder_Circuit.py
+++ 04-Computer-Architecture/01_Creating_an_Adder_Circuit.py
@@ -27,5 +27,45 @@ def XOR_gate(a, b):
 
 #---------------------------------------------------
 # Adder Circuit
+# Half Adder
+def half_adder(a, b):
+  s = XOR_gate(a, b)
+  c = AND_gate(a, b)
+  return s, c
+
+# TEST HALF ADDER 
+print(half_adder(0,0))
+print(half_adder(0,1))
+print(half_adder(1,0))
+print(half_adder(1,1))
+
+# Full Adder
+def full_adder(a, b, c):
+  s1 = XOR_gate(a,b) 
+  s2 = XOR_gate(s1, c)
+  c1 = AND_gate(a, b)
+  c2 = AND_gate(s1, c)
+  c_out = OR_gate(c1, c2)
+  return s2, c_out
+
+# TEST FULL ADDER
+print(full_adder(0,0,0))
+print(full_adder(1,1,1))
+print(full_adder(0,1,1))
+print(full_adder(1,1,0))
+
+# ALU
+def ALU(a, b, c, opcode):
+  if opcode == 0:
+    return half_adder(a, b)
+  else:
+    return full_adder(a, b, c)
+
+  
+# TEST ALU 
+print(ALU(0, 0, 1, 0))
+print(ALU(0,0,1,1))
+print(ALU(1, 1, 1, 0))
+print(ALU(1, 1, 1, 1))
 
 
